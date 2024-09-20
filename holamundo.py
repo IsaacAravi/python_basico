0340c27f5a20a38e1d8ca154d828a998ac02f028
IsaacAravi <gallo3090@gmail.com>
2024-09-20 11:28:19 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,21 @@
+"""
+UMG
+10° cuatrimestre
+"""
+'''
+Isaac Araujo
+'''
+
+#Practica 1
+#hola mundo
+
+print("Hola mundo")
+print('otra vez hola mundo')
+
+#como consultar el tipo de dato
+print(type("Soy un dato str"))#tipo 'str'
+print(type(5)) #tipo 'int'
+print(type(1.5)) #tipo 'float'
+print(type(3 + 1j)) # Tipo 'Complex'
+print(type(True)) # tipo'bool'
+print(type(print("Mi cadena de texto"))) #tipo 'NoneType'
